13540c2a8b9475800ef8e198f675c7f7238e5f21
danychaddad <dany.chaddad@gmail.com>
2022-12-10 12:16:33 +0200

    Created list.js boilerplate

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const bcrypt = require('bcrypt')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const listRouter = require('./routes/list');
 
 const app = express();
 
@@ -20,8 +21,10 @@ app.use(cookieParser());
 app.use(express.urlencoded({
   extended: true
 }));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/list', listRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
