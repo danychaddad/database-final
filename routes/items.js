1e26f3fbaff5d4683283b3127885bed4fd7f59d5
danychaddad <dany.chaddad@gmail.com>
2022-12-10 13:46:34 +0200

    Get item by ID

--- routes/items.js
+++ routes/items.js
@@ -5,8 +5,14 @@ const util = require('util');
 
 const query = util.promisify(con.query).bind(con);
 
-router.get('/:id', function (req, res) {
-    res.send(req.params.id);
+router.get('/:id', async function (req, res) {
+    const itemId = req.params.id;
+    const respon = await query('SELECT sellerId, name, description FROM product WHERE productId = ?', [itemId])
+    if (await respon.length == 0) {
+        res.send("No such item").end();
+    } else {
+        res.send(respon[0]);
+    }
 })
 
 module.exports = router;
