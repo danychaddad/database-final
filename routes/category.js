95e79be68b3890af56f4bfa2c509535fc5c10b10
danychaddad <dany.chaddad@gmail.com>
2022-12-13 17:20:04 +0200

    Can now create category

---
+++ routes/category.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const con = require('../util/database');
+const util = require('util');
+const query = util.promisify(con.query).bind(con);
+
+router.get('/all', async function (req, res) {
+    const respon = await query('SELECT productCategoryId, parentCategoryId, categoryName FROM product_category WHERE dateDeleted IS null');
+    res.send(await respon);
+})
+
+router.get('/:id', async function (req, res) {
+
+})
+
+router.post('/new', async function (req, res) {
+    const { parentCatName, catName } = req.body;
+    const parentCatId = await getCatId(parentCatName);
+    try {
+        await query('INSERT INTO product_category (parentCategoryId, categoryName) VALUES (?,?)', [parentCatId, catName]);
+    } catch (e) {
+        console.log(e);
+        return res.send("Something went wrong!");
+    }
+    return res.send(`Successfully added category ${catName}`);
+})
+
+let getCatId = async (catName) => {
+    const respon = await query('SELECT productCategoryId FROM product_category WHERE categoryName = ?', [catName]);
+    if (await respon.length == 0) {
+        return null;
+    }
+    return respon[0].productCategoryId;
+}
+
+module.exports = router;
