e40bdb98c8783b065fbd60210cf86963840605e1
Jad <jbalout@gmail.com>
2022-12-18 22:55:09 +0200

    db changes

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ con = () => {
     const db = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "",
+        password: "password",
         database: "markit"
     })
     return db;
